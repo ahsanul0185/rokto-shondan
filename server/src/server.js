ffa10f6664d866de84f561d9dcc9e24b59250f88
ahsanul0185 <ahsanul0185@gmail.com>
2024-11-29 21:58:29 +0600

    port modified

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const connectDB = require('./config/db');
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
