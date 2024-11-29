34863f17a300d5b2f82f3fbcc770665e6ff6abcc
ahsanul0185 <ahsanul0185@gmail.com>
2024-11-29 22:38:48 +0600

    final solution

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    // origin: "https://rokto-shondan.vercel.app", // Replace with your frontend URL
+    origin: "http://localhost:5173" || "https://rokto-shondan-6u7pwck5n-ahsanul-haques-projects.vercel.app", // Replace with your frontend URL
     credentials: true,              // Allow cookies to be sent
 }));
 app.use(cookieParser());
