8e29461946c373b30c2f4d628bac1b63534cb96d
ahsanul0185 <ahsanul0185@gmail.com>
2024-11-29 22:21:19 +0600

    modified cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://rokto-shondan.vercel.app/", // Replace with your frontend URL
+    origin: "https://rokto-shondan.vercel.app", // Replace with your frontend URL
     credentials: true,              // Allow cookies to be sent
 }));
 app.use(cookieParser());
